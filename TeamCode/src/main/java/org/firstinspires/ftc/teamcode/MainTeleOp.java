871aa7736cbd2ee59ac49b5a52d11f3ee6e120d5
Vignesh Suresh Kumar <vigneshsureshkumar17@gmail.com>
2021-09-04 18:03:23 -0400

    First commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Basics")
+public class MainTeleOp extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
